a0b178a73e3035bae0b975f7f655aa7849d77b42
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-07-30 19:44:23 +0530

    new commit

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,7 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './Student.css'
 
 function Student() {
-  const [data] = useState([
+  const initialFormState = {
+    name: '',
+    age: '',
+    course: '',
+    batch: '',
+    change: 'Edit',
+  };
+
+  const [data, setData] = useState([
     {
       name: 'Ajay',
       age: 22,
@@ -9,57 +18,57 @@ function Student() {
       batch: 'June',
       change: 'Edit',
     },
-    {
-      name: 'Vjay',
-      age: 24,
-      course: 'MERN',
-      batch: 'November',
-      change: 'Edit',
-    },
-    {
-      name: 'Ram',
-      age: 23,
-      course: 'MERN',
-      batch: 'August',
-      change: 'Edit',
-    },
-    {
-      name: 'Shyam',
-      age: 21,
-      course: 'MERN',
-      batch: 'April',
-      change: 'Edit',
-    },
-    {
-      name: 'Raj',
-      age: 20,
-      course: 'MERN',
-      batch: 'March',
-      change: 'Edit',
-    },
-    {
-      name: 'Dev',
-      age: 25,
-      course: 'MERN',
-      batch: 'September',
-      change: 'Edit',
-    },
-    {
-      name: 'Pawan',
-      age: 21,
-      course: 'MERN',
-      batch: 'February',
-      change: 'Edit',
+    // ... other student data ...
+  ]);
+
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState(initialFormState);
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleAddNewStudent = () => {
+    setFormData(initialFormState);
+    setShowForm(true);
+    setEditIndex(null);
+  };
+
+  const handleEditStudent = (index) => {
+    setFormData(data[index]);
+    setShowForm(true);
+    setEditIndex(index);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.name && formData.age && formData.course && formData.batch) {
+      if (editIndex === null) {
+        // Add new student
+        setData([...data, formData]);
+      } else {
+        // Edit existing student
+        const updatedData = [...data];
+        updatedData[editIndex] = formData;
+        setData(updatedData);
+      }
+      setShowForm(false);
+      setEditIndex(null);
     }
-  ])
+  };
+
   return (
     <div>
       <div className="container">
         <div className="main">
           <h2>Students Details</h2>
-          <button className='add-new-student'>Add New Student</button>
+          <button className="add-new-student" onClick={handleAddNewStudent}>
+            Add New Student
+          </button>
         </div>
-        <table className='students-table' border='1px'>
+        <table className="students-table" border="1px">
           <tr>
             <th>Name</th>
             <th>Age</th>
@@ -67,23 +76,44 @@ function Student() {
             <th>Batch</th>
             <th>Change</th>
           </tr>
-          {
-            data.map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item.name}</td>
-                  <td>{item.age}</td>
-                  <td>{item.course}</td>
-                  <td>{item.batch}</td>
-                  <td className='edit-item'>{item.change}</td>
-                </tr>
-              )
-            })
-          }
+          {data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.course}</td>
+                <td>{item.batch}</td>
+                <td className="edit-item" onClick={() => handleEditStudent(index)}>
+                  {item.change}
+                </td>
+              </tr>
+            );
+          })}
         </table>
       </div>
+
+      {showForm && (
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <label>Name:</label>
+            <input type="text" name="name" value={formData.name} onChange={handleChange} />
+            <br />
+            <label>Age:</label>
+            <input type="number" name="age" value={formData.age} onChange={handleChange} />
+            <br />
+            <label>Course:</label>
+            <input type="text" name="course" value={formData.course} onChange={handleChange} />
+            <br />
+            <label>Batch:</label>
+            <input type="text" name="batch" value={formData.batch} onChange={handleChange} />
+            <br />
+            <button type="submit">{editIndex === null ? 'Add' : 'Update'}</button>
+            <button type="button" onClick={() => setShowForm(false)}>Cancel</button>
+          </form>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Student
+export default Student;
